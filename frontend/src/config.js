e621b6c74e5798a36b290d9c12af534ace77a6c2
Chavisky D <auukunn.bkk@gmail.com>
2026-02-19 15:58:00 +0700

    chore: set production LIFF and GAS config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const LIFF_ID = "YOUR_LIFF_ID";
+export const LIFF_ID = "2009175111-a2oh3LFi";
 export const GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxJod_vEpIdI75lOYdAbsRsbYMaF5kOyRA2r-2C5hnbVbCk8H0g9Wq2Hmu8VsiG-U0u3w/exec";
 export const DEV_LINE_USER_ID = "Udev-please-replace";
 export const GOOGLE_CLIENT_ID = "REPLACE_ME";
