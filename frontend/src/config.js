d9626107b8f22573c2b27addba67c73c5820bd68
Chavisky D <auukunn.bkk@gmail.com>
2026-02-19 17:26:27 +0700

    add google clients

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,7 @@
 export const LIFF_ID = "2009175111-a2oh3LFi";
 export const GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxJod_vEpIdI75lOYdAbsRsbYMaF5kOyRA2r-2C5hnbVbCk8H0g9Wq2Hmu8VsiG-U0u3w/exec";
 export const DEV_LINE_USER_ID = "Udev-please-replace";
-export const GOOGLE_CLIENT_ID = "REPLACE_ME";
-
+export const GOOGLE_CLIENT_ID = "67748418977-3213lcgvieesjc7f0ql4uc36pdrt6d8u.apps.googleusercontent.com";
 export const APP_CONFIG = {
   LIFF_ID,
   GAS_WEBAPP_URL,
